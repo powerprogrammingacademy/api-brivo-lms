d0da2df7aa6b8841f812a80f1c453fcc56286d46
Maria Jhosé Garcia <mariajhosegarcia@gmail.com>
2026-06-23 19:55:22 -0600

    agregar variable de entorno

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
-include 'controllers/ruta.controller.php';
+include 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
+include 'controllers/ruta.controller.php';
 $ruta = new RutaController();
 $ruta->index();
