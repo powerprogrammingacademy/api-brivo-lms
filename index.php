47d8f5e351cf8690a80ffbd6f4568737c61416ab
Maria Jhosé Garcia <mariajhosegarcia@gmail.com>
2026-06-23 20:05:50 -0600

    configurar cabeceras y agregar ejemplo de .env

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-
+// carga las variables de entorno
 include 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+// permite el acceso a la api desde cualquier origen
+header("Access-Control-Allow-Origin: " . $_ENV['APP_URL']);
+// permite los métodos http
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+// permite las cabeceras
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include 'controllers/ruta.controller.php';
 $ruta = new RutaController();
